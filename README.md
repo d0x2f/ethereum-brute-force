b25f3009c427d437426f0d211b0297f8f8776761
Dylan McGannon <peltium@gmail.com>
2021-04-30 17:03:06 +1000

    readme and tweaks

---
+++ README.md
@@ -0,0 +1,53 @@
+# Ethereum Random Private Key Brute Force Searcher
+
+This small project will continuously generate random ethereum private keys and log any that have a balance on the ethereum mainnet.
+
+In other words, this project will stress test your CPU and not find any ethereum.
+
+## To Run
+
+You must have a fully synchronised geth node for this script to run against.
+
+It should preferably be running on localhost to reduce latency as much as possible.
+
+It will be very slow running against a `--syncmode light` node to the point where it could be faster refreshing the page at https://keys.lol.
+
+To install dependencies run:
+
+```sh
+$ npm install
+```
+
+And to start the script run:
+
+```sh
+$ npm start
+```
+
+You'll see output like the following:
+
+```sh
+$ node main.js
+Checked: 722 @ 722/s, Found: 0
+Checked: 1562 @ 840/s, Found: 0
+Checked: 2422 @ 860/s, Found: 0
+Checked: 3342 @ 920/s, Found: 0
+Checked: 4262 @ 920/s, Found: 0
+Checked: 5192 @ 930/s, Found: 0
+Checked: 6142 @ 950/s, Found: 0
+```
+
+If any accounts with balance are found (they won't be), you can see them logged the 'wallets' file:
+
+```
+$ cat wallets
+cat: wallets: No such file or directory
+```
+
+## Indicative Performance
+
+When I run this on my PC (i7-6700K), I get a check rate of around 1500/s.
+
+On an n2-standard-4 instance I see ~1000/s.
+
+I haven't tried but you may be able to run multiple instances to improve the check rate since nodejs is single threaded.
---
+++ main.js
@@ -0,0 +1,86 @@
+const { writeFile } = require("fs").promises;
+const Web3 = require("web3");
+const { randomBytes } = require("crypto");
+const Queue = require("queue-promise");
+const CONCURRENCY = 100;
+
+async function recordFind(key, account, balance) {
+  writeFile(
+    "wallets",
+    `Key: ${key}, Address: ${account.address}, Balance: ${balance} gwei\n`,
+    { flag: "a" }
+  );
+}
+
+async function checkRandomKey(web3) {
+  // Generate a random 32 byte key
+  const key = "0x" + randomBytes(32).toString("hex");
+
+  // Fetch the corresponding account
+  const account = web3.eth.accounts.privateKeyToAccount(key);
+
+  // Check it's balance
+  const balance = await web3.eth.getBalance(account.address);
+
+  // If it has a balance, record it
+  if (balance > 0) {
+    await recordFind(key, account, balance);
+    return true;
+  }
+  return false;
+}
+
+async function main() {
+  // Connects to geth on localhost via websocket
+  // (start geth with `--ws --ws.api eth,net,web3`)
+  const web3 = new Web3("ws://localhost:8546");
+
+  let total = 0;
+  let checked = 0;
+  let found = 0;
+
+  const queue = new Queue({
+    concurrent: CONCURRENCY,
+    interval: 0,
+    start: true,
+  });
+
+  // Print the test rate each second
+  const timer = setInterval(() => {
+    total += checked;
+    console.error(`Checked: ${total} @ ${checked}/s, Found: ${found}`);
+    checked = 0;
+  }, 1000);
+
+  const exit = () => {
+    clearInterval(timer);
+    web3.currentProvider.disconnect();
+    process.exit();
+  };
+
+  process.on("SIGINT", exit);
+  queue.on("stop", exit);
+
+  // When a job is completed, start another one
+  queue.on("resolve", (hasBalance) => {
+    checked++;
+    if (hasBalance) {
+      found++;
+    }
+    queue.enqueue(() => checkRandomKey(web3));
+  });
+
+  // When a job fails, log it and quit.
+  queue.on("reject", (error) => {
+    console.error(error);
+    queue.enqueue(() => checkRandomKey(web3));
+    exit();
+  });
+
+  // Enqueue initial jobs
+  for (i = 0; i < CONCURRENCY; i++) {
+    queue.enqueue(() => checkRandomKey(web3));
+  }
+}
+
+main();
